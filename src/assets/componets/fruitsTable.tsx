11eee67cd4a378dc998770866ba2ff30cc9be25c
Batuhan Çoban <batuhancoban@Batuhan-MacBook-Pro.local>
2024-05-30 00:42:10 +0300

    fritsTables.tsx dosyası yaıldı

---
+++ src/assets/componets/fruitsTable.tsx
@@ -0,0 +1,42 @@
+import { TableWrapperProps, FruitsOrVeggies } from './types';
+
+export const FruitsTable = ({
+  inputValue,
+  isProductInStock,
+}: TableWrapperProps) => {
+  const fruits: FruitsOrVeggies = [
+    { id: 0, name: 'Elma', price: '21₺', inStock: true },
+    { id: 1, name: 'Armut', price: '32₺', inStock: true },
+    { id: 2, name: 'Kiraz', price: '53₺', inStock: false },
+    { id: 3, name: 'Erik', price: '66₺', inStock: true },
+    { id: 4, name: 'Muz', price: '26₺', inStock: false },
+    { id: 5, name: 'Çilek', price: '43₺', inStock: true },
+    { id: 6, name: 'Nektarin', price: '31₺', inStock: true },
+    { id: 7, name: 'Vişne', price: '62₺', inStock: false },
+    { id: 8, name: 'Ananas', price: '83₺', inStock: true },
+  ];
+
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>Fruits</th>
+          </tr>
+        </thead>
+        <tbody>
+          {fruits.filter((fruit) => {
+            const productFilter =  fruit.name.toLowerCase().includes(inputValue.toLowerCase());
+            const stockControl = !isProductInStock || fruit.inStock;
+            return productFilter && stockControl;
+          }).map((fruit ) => (
+            <tr key={fruit.id}>
+              <td>{fruit.name} {fruit.price}</td>
+            </tr>
+          ))
+          }
+        </tbody>
+      </table>
+    </>
+  );
+};
